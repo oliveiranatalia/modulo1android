92345da6431b51ef09ce0d53ce7b87f939e25aad
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-05-25 20:23:13 -0300

    segundo exercicio

--- 36/MeuPet/app/src/main/java/br/com/zup/meupet/MainActivity.kt
+++ 36/MeuPet/app/src/main/java/br/com/zup/meupet/MainActivity.kt
@@ -2,10 +2,17 @@ package br.com.zup.meupet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botao = findViewById<Button>(R.id.botao)
+
+        botao.setOnClickListener{
+            Toast.makeText(this,"Eu sou o miguelzito!!",Toast.LENGTH_LONG).show()
+        }
     }
 }
