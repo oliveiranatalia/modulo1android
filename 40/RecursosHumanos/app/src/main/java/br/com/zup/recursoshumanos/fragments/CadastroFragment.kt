e60b5ddcd3bdcc4f8e3e24e764b705aeb1b27720
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-14 19:19:31 -0300

    Implementar logica pra navigation

--- 40/RecursosHumanos/app/src/main/java/br/com/zup/recursoshumanos/fragments/CadastroFragment.kt
+++ 40/RecursosHumanos/app/src/main/java/br/com/zup/recursoshumanos/fragments/CadastroFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.NavHostFragment
+import br.com.zup.recursoshumanos.ERROR
+import br.com.zup.recursoshumanos.KEY
+import br.com.zup.recursoshumanos.R
 import br.com.zup.recursoshumanos.databinding.FragmentCadastroBinding
+import br.com.zup.recursoshumanos.model.Funcionario
 
 class CadastroFragment : Fragment() {
     private lateinit var binding:FragmentCadastroBinding
+    private lateinit var nome:String
+    private lateinit var horas:String
+    private lateinit var valor:String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,14 +26,39 @@ class CadastroFragment : Fragment() {
         binding = FragmentCadastroBinding.inflate(inflater,container,false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.botao.setOnClickListener{
-            getFunc()
+            sendInfo()
         }
     }
-    private fun getFunc(){
-
+    private fun getInfo(){
+        this.nome = binding.etNome.text.toString()
+        this.horas = binding.etHoras.text.toString()
+        this.valor = binding.etValor.text.toString()
+    }
+    private fun sendInfo(){
+        val func = checkInfo()
+        if(func != null){
+            val bundle = bundleOf(KEY to func)
+            NavHostFragment.findNavController(this).navigate(R.id.action_cadastroFragment_to_informacoesFragment,bundle)
+        }
+    }
+    private fun checkInfo():Funcionario?{
+        getInfo()
+        return if(nome.isNotEmpty() || nome.isNotBlank() && horas.isNotEmpty() || horas.isNotBlank() && valor.isNotEmpty() || valor.isNotBlank()) {
+            clear()
+            Funcionario(nome, horas.toInt(), valor.toDouble())
+        }else{
+            binding.etNome.error = ERROR
+            binding.etHoras.error = ERROR
+            binding.etValor.error = ERROR
+            null
+        }
+    }
+    private fun clear(){
+        binding.etNome.text.clear()
+        binding.etValor.text.clear()
+        binding.etHoras.text.clear()
     }
 }
