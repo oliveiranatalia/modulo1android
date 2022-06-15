fececd1443ddbfeec54f6f1e6ce9a1b93f796531
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-15 01:13:59 -0300

    Inserir requisitos de negocio

---
+++ 42/Zoologico/app/src/main/java/br/com/zup/zoologico/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.zoologico.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.zoologico.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
