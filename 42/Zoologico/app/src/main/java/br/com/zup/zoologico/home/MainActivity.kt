c76dc6d323947036b5465ed8ed3926ab770900d1
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-20 09:50:34 -0300

    Inserir requisitos de negocio e habilitar binding, navigation e parcelize

--- 42/Zoologico/app/src/main/java/br/com/zup/zoologico/home/MainActivity.kt
+++ 42/Zoologico/app/src/main/java/br/com/zup/zoologico/home/MainActivity.kt
@@ -2,11 +2,28 @@ package br.com.zup.zoologico.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import br.com.zup.zoologico.R
+import br.com.zup.zoologico.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        appBar()
+        toolbar()
+    }
+    private fun appBar(){
+        supportActionBar?.hide()
+    }
+    private fun toolbar(){
+        val navHostFragment = (supportFragmentManager.findFragmentById(binding.navHostFragment.id)) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController,appBarConfiguration)
     }
 }
