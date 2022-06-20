8cf46c4dfcf4584a1130b19ffc323d62ea71a227
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-06-19 23:42:19 -0300

    Inserir requisitos de negocio e habilitar binding, navigition e parcelize

--- 42/Zoologico/app/src/main/java/br/com/zup/zoologico/home/MainActivity.kt
+++ 42/Zoologico/app/src/main/java/br/com/zup/zoologico/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.zup.zoologico
+package br.com.zup.zoologico.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.zoologico.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
