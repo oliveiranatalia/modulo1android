9f425f007743c6e95bca0cd13e109246bec7bc0c
oliveiranatalia <102336871+oliveiranatalia@users.noreply.github.com>
2022-06-21 16:40:16 -0300

    Delete 43/MusicaFavoritaII directory

--- 43/MusicaFavoritaII/app/src/main/java/br/com/zup/musicafavorita/model/Banda.kt
+++
@@ -1,9 +0,0 @@
-package br.com.zup.musicafavorita.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class Banda (private val imagem:Int):Parcelable{
-    fun getImage() = this.imagem
-}
